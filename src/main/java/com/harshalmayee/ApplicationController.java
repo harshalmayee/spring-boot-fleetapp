9ebbe9b28e543d7c001d03bccb602ff3d6122d63
Harshal Mayee <harshalmayee1288@gmail.com>
2021-06-09 12:06:23 +0530

    Profile change

--- src/main/java/com/harshalmayee/ApplicationController.java
+++ src/main/java/com/harshalmayee/ApplicationController.java
@@ -14,12 +14,12 @@ public class ApplicationController {
 
     @GetMapping("/login")
     public String login(){
-        return "login";
+        return "login1";
     }
 
     @GetMapping("/logout")
     public String logout(){
-        return "login";
+        return "login1";
     }
 
     @GetMapping("/register")
